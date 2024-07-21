046fefcafdc825d65830ae78dc7b2358df01c19f
fahedsayyed <sayyed.brisklogic@gmail.com>
2024-07-22 03:29:40 +0530

    all changes modified according to given pixel in documents

--- asset/js/script.js
+++ asset/js/script.js
@@ -22,3 +22,52 @@ let SwiperTop = new Swiper(".leader_logo", {
     },
   },
 });
+
+
+// testimonial
+    var SwiperTest = new Swiper(".testimonial", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      autoplay: true,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+// let SwiperTest = new Swiper(".testimonial", {
+//   // spaceBetween: 0,
+//   // centeredSlides: true,
+//   speed: 4000,
+//   autoplay: {
+//     delay: 0.8,
+//   },
+//   loop: true,
+//   //   slidesPerView: "auto",
+//   slidesPerView: 1,
+//   // allowTouchMove: false,
+//   // disableOnInteraction: true,
+//   // breakpoints: {
+//   //   480: {
+//   //     slidesPerView: 3,
+//   //   },
+//   //   768: {
+//   //     slidesPerView: 4,
+//   //   },
+//   //   992: {
+//   //     slidesPerView: 6,
+//   //   },
+//   // },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
